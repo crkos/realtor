b0036bb9aa151ed968937b635cecabc7c24055d8
crkos <jordandark123@gmail.com>
2023-06-19 20:37:39 -0700

    Fix routing vercel

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -87,7 +87,7 @@ const Category = () => {
                         </div>
                     )}
                 </>
-            ) : <p>There are no current offers</p>
+            ) : <p>There are no current {params.categoryName === "rent" ? "places for rent" : "places for sale"}</p>
             }
         </div>
     );
